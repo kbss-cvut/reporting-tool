0c96d95dbc12b282b7a4cab88686bc36aabe2395
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2016-07-07 16:09:11 +0200

    Added license headers.

--- src/main/java/cz/cvut/kbss/reporting/service/jmx/AppAdminBean.java
+++ src/main/java/cz/cvut/kbss/reporting/service/jmx/AppAdminBean.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (C) 2016 Czech Technical University in Prague
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package cz.cvut.kbss.reporting.service.jmx;
 
 import cz.cvut.kbss.reporting.service.event.InvalidateCacheEvent;
@@ -8,7 +22,7 @@ import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Component;
 
 @Component
-@ManagedResource(objectName = "bean:name=INBASStageAdminBean", description = "Application administration bean. ")
+@ManagedResource(objectName = "bean:name=AdminBean", description = "Application administration bean. ")
 public class AppAdminBean implements ApplicationEventPublisherAware {
 
     private ApplicationEventPublisher eventPublisher;
